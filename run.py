1ec32453668a5530d8bcf170d09db3dd7fcacbc7
Arcandrus <eric.harper87@gmail.com>
2025-03-13 15:42:45 +0000

    Added vaildation to a while loop to request data until valid data is entered.

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+"https://www.googleapis.com/auth/spreadsheets",
+"https://www.googleapis.com/auth/drive.file",
+"https://www.googleapis.com/auth/drive"
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -13,29 +13,38 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
-    """
-    Get sales data from user
-    """
-    print('Please enter sales data from last market.')
-    print('Data should be six numbers separated by commas.')
-    print('Example: 10,20,30,40,50,60\n')
-    
-    data_str = input('Enter your data here: ')
-    
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+	"""
+	Get sales data from user
+	"""
+	while True:
+		print('Please enter sales data from last market.')
+		print('Data should be six numbers separated by commas.')
+		print('Example: 10,20,30,40,50,60\n')
+
+		data_str = input('Enter your data here: ')
+
+		sales_data = data_str.split(',')
+
+		if validate_data(sales_data):
+			print('Data is valid')
+			break
+	
+	return sales_data
 
 def validate_data(values):
-    """
-    Validate input calues are correct
-    """
-    try:
-        [int(value) for value in values]
-        if len(values) != 6:
-            raise ValueError(
-				(f'Exactly 6 values must be entered. You entered {len(values)}')
+	"""
+	Validate input values are correct
+	"""
+	try:
+		[int(value) for value in values]
+		if len(values) != 6:
+			raise ValueError(
+			(f'Exactly 6 values must be entered. You entered {len(values)}')
 			)
-    except ValueError as e:
-        print(f'Invalid data: {e}. Please try again.')
+	except ValueError as e:
+		print(f'Invalid data: {e}. Please try again.')
+		return False
 
-get_sales_data()
+	return True
+
+data = get_sales_data()
